07fd5b10868f007b5883a7e5385c63518a3d34f5
Dmitriy Matviiets <dmatviiets@wscad.com>
2022-02-13 20:22:47 +0100

    header component

--- DeviceViewer/ClientApp/src/app/app.module.ts
+++ DeviceViewer/ClientApp/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent,
     HomeComponent,
+    HeaderComponent,
     CounterComponent,
     FetchDataComponent
   ],
---
+++ DeviceViewer/ClientApp/src/app/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  template: `<h1>Probeaufgabe</h1><h1>- Dmitriy Matviiets -</h1> - <h1>[Datum]</h1>`
+})
+export class HeaderComponent {
+}
